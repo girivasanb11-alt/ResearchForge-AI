ebce85bef373262040f627ed0e0f7c50eaf7313a
Girivasan <girivasanb11@gmail.com>
2026-08-27 08:00:43 +0530

    refactor: clean unused imports for pristine production build

--- src/components/home/BenchmarkMatrix.tsx
+++ src/components/home/BenchmarkMatrix.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export function BenchmarkMatrix() {
   return null;
 }
--- src/components/home/PipelineVisualizer.tsx
+++ src/components/home/PipelineVisualizer.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export function PipelineVisualizer() {
   return null;
 }
